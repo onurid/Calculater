389ff21ad9e82feeac29e283ed84064e4dc24558
onuryasar34 <onuryasar34@gmail.com>
2015-07-31 11:52:00 +0300

    Include files

---
+++ ConsoleApps/ConsoleApps.Utils/Password.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApps.Utils
+{
+    class Password
+    {
+        public static string GeneretaPassword()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var random = new Random();
+            var result = new string(
+                Enumerable.Repeat(chars, 8)
+                          .Select(s => s[random.Next(s.Length)])
+                          .ToArray());
+            return result;
+
+            return "";
+        }
+    }
+}
