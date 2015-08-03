d5a840c0e23798e03750638a68abf0ed9e055057
onuryasar34 <onuryasar34@gmail.com>
2015-08-03 15:44:40 +0300

    Delete All Project

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Calculator
+{
+    public static class Calculator
+    {
+        private static double _val1 = 0.0;
+        private static double _val2 = 0.0;
+        private static string _process = null;
+
+        public static string Val1
+        {
+            get { return _val1.ToString(); }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                    _val1 = Convert.ToDouble(value);
+            }
+        }
+        public static string Val2
+        {
+            get { return _val2.ToString(); }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                    _val2 = Convert.ToDouble(value);
+            }
+        }
+        public static string Process
+        {
+            get { return _process; }
+            set { _process = value; }
+        }
+
+        public static double Calculate()
+        {
+            switch (Process)
+            {
+                case "+":
+                    return Collect();
+                    
+                case "-":
+                    return Interest();
+
+                case "*":
+                    return Multiply();
+
+                case "/":
+                    return Divide();
+
+                default:
+                    return (double) 0.0;
+            }
+        }
+
+        private static double Collect()
+        {
+            return _val1 + _val2;
+        }
+        private static double Interest()
+        {
+            return _val1 - _val2;
+        }
+        private static double Multiply()
+        {
+            return _val1 * _val2;
+        }
+        private static double Divide()
+        {
+            return _val1 / _val2;
+        }
+    }
+}
---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        private const string txt = "Sayi: ";
+
+        static void Main(string[] args)
+        {
+            Console.Write(txt);
+            Calculator.Val1 = Console.ReadLine();
+
+            Console.Write("Islem ([+], [-], [*], [/]): ");
+            Calculator.Process = Console.ReadLine();   
+            
+            Console.Write(txt);
+            Calculator.Val2 = Console.ReadLine();
+            
+            Console.Write(string.Format("Sonuc: {0}",Calculator.Calculate()));
+            
+            Console.ReadKey();
+        }
+    }
+}
