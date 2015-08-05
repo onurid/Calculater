976f357ea087849e9573a645f95093c912bb0063
onuryasar34 <onuryasar34@gmail.com>
2015-08-05 16:21:42 +0300

    add Interface

---
+++ Calculator/Calculator/ICalculator.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Calculator
+{
+    public interface ICalculator
+    {
+        double FirstNumber { get; set; }
+        double SecondNumber { get; set; }
+        string Process { get; set; }
+
+        void Calculator(string process, double firstNumber, double secondNumber);
+
+        double Calculate();
+
+        double Collect();
+        double Interest();
+        double Multiply();
+        double Divide();
+    }
+}
